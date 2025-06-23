e39c2cd529d6c8713807e7627fddda6c6557b492
Mateusz Gruntkowski <mat.gruntkowski@gmail.com>
2025-06-23 12:09:25 +0200

    refactor: comment out UserLessonProgressRepository code for future reference

--- backend/src/main/java/com/eb/language_self_study/repository/UserLessonProgressRepository.java
+++ backend/src/main/java/com/eb/language_self_study/repository/UserLessonProgressRepository.java
@@ -1,10 +1,10 @@
-package com.eb.language_self_study.repository;
-
-import com.eb.language_self_study.model.UserLessonProgress;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserLessonProgressRepository extends JpaRepository<UserLessonProgress, Long> {
-
-}
+//package com.eb.language_self_study.repository;
+//
+//import com.eb.language_self_study.model.UserLessonProgress;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface UserLessonProgressRepository extends JpaRepository<UserLessonProgress, Long> {
+//
+//}
